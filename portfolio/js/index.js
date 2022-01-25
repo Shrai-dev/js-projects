0cbbef6c63aa114dfc028053c8286344726aff1a
Anastasia <nastia.shrai@gmail.com>
2022-01-25 15:04:13 +0200

    feat: implement changing seasons in portfolio section

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -2,6 +2,11 @@ const burgerIcon = document.querySelector('.burger__menu');
 const navigationMenu = document.querySelector('.header__nav-list');
 const navigationItems = document.querySelectorAll('.header__nav-link');
 
+const portfolioBtns = document.querySelector('.portfolio__wrapper-btn');
+const portfolioBtn = document.querySelectorAll('.portfolio__btn');
+const portfolioImages = document.querySelectorAll('.portfolio__items-image');
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
 if (burgerIcon) {
   burgerIcon.addEventListener('click', () => {
     open();
@@ -21,5 +26,34 @@ function close() {
     burgerIcon.classList.remove('active');
   }
 }
+
+function changeImage(e) {
+  if (e.target.classList.contains('portfolio__btn')) {
+    portfolioImages.forEach(
+      (img, index) =>
+        (img.src = `./assets/images/${e.target.dataset.season}/${
+          index + 1
+        }.jpg`)
+    );
+  }
+}
+
+function changeClassActiveBtn(e) {
+  portfolioBtn.forEach((el) => el.classList.remove('btn-active'));
+  e.target.classList.add('btn-active');
+}
+
+portfolioBtns.addEventListener('click', changeImage);
+portfolioBtns.addEventListener('click', changeClassActiveBtn);
+
+function preloadSummerImages() {
+  seasons.forEach((el) => {
+    for (let i = 1; i <= 6; i++) {
+      const img = new Image();
+      img.src = `./assets/images/${el}/${i}.jpg`;
+    }
+  });
+}
+preloadSummerImages();
 
 console.log(`Оценка - 75 баллов`);
