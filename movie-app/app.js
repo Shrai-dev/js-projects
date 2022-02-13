3d1f97b2de094fc68c5b62919b8693fd21b8c03b
Anastasia <nastia.shrai@gmail.com>
2022-02-13 15:57:38 +0200

    feat: implement search on the page

--- movie-app/app.js
+++ movie-app/app.js
@@ -1,12 +1,17 @@
+const searchBar = document.getElementById('search-bar');
 const mainContent = document.querySelector('.main-content');
 const APIKey = '1522de262832f8a37e8d96e7ed928ba5';
+// const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKey}&language=en-US`;
+
 const popular = `https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`;
 
 async function getData() {
+  // const res = await fetch(url);
   const res = await fetch(popular);
   const data = await res.json();
   console.log(data);
   console.log(data.results[0]['backdrop_path']);
+  // console.log(data.genres[0]);
   renderCards(data);
 }
 
@@ -23,4 +28,22 @@ function renderCards(data) {
     </div>`;
     mainContent.append(container);
   });
+}
+
+searchBar.addEventListener('input', (event) => {
+  if (event.target.value !== '') {
+    doSearch();
+  } else {
+    getData();
+  }
+});
+
+async function doSearch() {
+  const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${APIKey}&query=${searchBar.value}&page=1`;
+  const res = await fetch(searchUrl);
+  const searchData = await res.json();
+  mainContent.innerHTML = '';
+  // console.log(searchData);
+
+  renderCards(searchData);
 }
