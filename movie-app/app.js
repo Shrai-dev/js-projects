f9909d0a6273adf277c92e51b9877250a42c66ce
Anastasia <nastia.shrai@gmail.com>
2022-02-13 23:05:52 +0200

    feat: add overview to page

--- movie-app/app.js
+++ movie-app/app.js
@@ -1,23 +1,19 @@
 const searchBar = document.getElementById('search-bar');
 const mainContent = document.querySelector('.main-content');
 const APIKey = '1522de262832f8a37e8d96e7ed928ba5';
-// const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKey}&language=en-US`;
 
 const popular = `https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`;
 
 async function getData() {
-  // const res = await fetch(url);
   const res = await fetch(popular);
   const data = await res.json();
-  console.log(data);
-  console.log(data.results[0]['backdrop_path']);
-  // console.log(data.genres[0]);
   renderCards(data);
 }
 
 getData();
 
 function renderCards(data) {
+  mainContent.innerHTML = '';
   data.results.forEach((el, index) => {
     const container = document.createElement('div');
     container.classList.add('movie-container');
@@ -25,7 +21,8 @@ function renderCards(data) {
     <div class='movie-info'>
     <h3 class='movie-title'>${data.results[index]['title']}</h3>
     <p class='movie-rating'>${data.results[index]['vote_average']}</p>
-    </div>`;
+    </div>
+    <div class='movie-overview'>${data.results[index]['overview']}</div>`;
     mainContent.append(container);
   });
 }
@@ -43,7 +40,5 @@ async function doSearch() {
   const res = await fetch(searchUrl);
   const searchData = await res.json();
   mainContent.innerHTML = '';
-  // console.log(searchData);
-
   renderCards(searchData);
 }
